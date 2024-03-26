49c5c8a5526bddd44225a2caabd37fe2af58fafb
sacholic4160 <sachinparmar4160@gmail.com>
2024-03-26 16:36:49 +0530

    project done

--- Controllers/form.controller.js
+++ Controllers/form.controller.js
@@ -3,14 +3,15 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import mongoose from "mongoose";
 
 const fillForm = asyncHandler(async (req, res) => {
   const { title, description } = req.body;
 
   const imgLocalPath = req.file?.path; // Access the uploaded file using req.file
 
-   console.log(imgLocalPath)
-   
+  console.log(imgLocalPath);
+
   if (!(title && description)) {
     throw new ApiError(404, "title and description both are required!!");
   }
@@ -23,7 +24,7 @@ const fillForm = asyncHandler(async (req, res) => {
   }
 
   const image = await uploadOnCloudinary(imgLocalPath);
-console.log(image)
+  console.log(image);
   if (!image) {
     throw new ApiError(
       400,
@@ -38,21 +39,90 @@ console.log(image)
     description,
     image: image.url,
   });
- console.log(form)
+  console.log(form);
   if (!form) {
     throw new ApiError(404, "there is some error while creating the form!");
   }
 
   await form.save();
 
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, form, "The form has been created successfully!")
+    );
+});
+
+//function for getting all form details
+const getFormDetails = asyncHandler(async (req, res) => {
+
+  const  _id = req.params.formId
+  //checking whether the given  formId exists or not
+  if (!_id) {
+    throw new ApiError(
+      404,
+      "Invalid form id OR form Id is missing in the request"
+    );
+  }
+
+  const form = await Form.findById({_id}) //: req.params.formId});
+  console.log(form);
 
   return res
-  .status(200)
-  .json(
-    new ApiResponse(200 , form , "The form has been created successfully!")
-  )
+    .status(200)
+    .json(new ApiResponse(201, form, "Form Details fetched Successfully"));
+});
+
+const updateForm = asyncHandler(async (req, res) => {
+  // const { title, description } = req.body;
+  // const _id = req.params.formId;
+
+  // Check if a file was uploaded
+  let image = {};
+  if (req.file) {
+    const imgLocalPath = req.file.path;
+    image = await uploadOnCloudinary(imgLocalPath);
+  }
+
+  // if (!_id) {
+  //   throw new ApiError(404, "Invalid form id OR formId is required!");
+  // }
+
+  // Convert _id to ObjectId if necessary
+
+
+  const form = await Form.findByIdAndUpdate(
+  {_id : req.params.formId},
+    {
+      $set: {
+        title: req.body.title,
+        description: req.body.description,
+        image: req.file ? image.url : undefined, // Update image only if a file was uploaded
+      },
+    },
+    { new: true }
+  );
+
+  return res
+    .status(200)
+    .json(new ApiResponse(201, form, "Form details updated successfully!"));
 });
 
 
 
-export {fillForm,}
+//function for deleting form details
+const deleteFormDetails = asyncHandler(async (req, res) => {
+  const  _id = req.params.formId; //here we cannnot destructure the _id 
+
+  if (!_id) {
+    throw new ApiError(400, "please provide a valid form Id");
+  }
+
+  const form = await Form.findByIdAndDelete({_id});
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(200, form, "The form has been deleted  succesfully!")
+    );
+});
+export { fillForm, updateForm, getFormDetails, deleteFormDetails };
--- Routes/form.route.js
+++ Routes/form.route.js
@@ -1,12 +1,24 @@
 import { Router } from "express";
-import { fillForm } from "../Controllers/form.controller.js";
+import { fillForm,updateForm, getFormDetails, deleteFormDetails } from "../Controllers/form.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/fill-form").post(
+//route for filling a form
+router.route("/fill-form/").post(
   upload.single("image"), // Corrected to use single field name
   fillForm
 );
+
+//route for getting form details
+ router.route("/get-details/:formId").get(getFormDetails)
+
+//route for updating a form
+router.route("/update-form/:formId").put(upload.single("image"),updateForm)
+
+//route for deleting form details 
+router.route("/delete-form/:formId").delete(deleteFormDetails)
+
+
 
 export default router;
